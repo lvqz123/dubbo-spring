79bb2b3748180f01575e0644a012e90c43559d4c
lvqz <qiuzhen_1726@163.com>
2019-10-15 22:49:15 +0800

    增加一个新的分支，用来使用注解来配置

--- dubbo-consumer/src/test/java/SpringDubboConsumerTest.java
+++ dubbo-consumer/src/test/java/SpringDubboConsumerTest.java
@@ -1,28 +1,21 @@
 import com.lvqz.common.service.IUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import javax.annotation.Resource;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:dubbo-consumer.xml")
 public class SpringDubboConsumerTest {
 
-
-
-
-//    @Autowired
-//    private IUserService userService;
+    @Resource
+    private IUserService userService;
 
     @Test
     public void test(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:dubbo-consumer.xml");
-        IUserService userService = (IUserService) context.getBean("helloService");
         System.out.println(userService.getUserById(1));
     }
 
